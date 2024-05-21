b932881c70ffeb10f2cf26ad2e577903ede4abea
Anvesh Mishra <anveshgreat11@gmail.com>
2024-05-22 00:03:45 +0530

    minor

--- utils/const.js
+++ utils/const.js
@@ -4,7 +4,10 @@ import spotify from './spotify.json'
 export const SOLANA_HOST = clusterApiUrl('devnet')
 
 export const STABLE_POOL_PROGRAM_ID = new PublicKey(
-  '5wSMdEYxSW7iB3rdE7c8yB3bqBUrVtvggjDdY5viyDLk',
+  'G67AaRd7VPZyM5m27wkuFZYKP3Jb68oLHA1GsUukxWs8',
 )
 
 export const STABLE_POOL_IDL = spotify
+
+//"G67AaRd7VPZyM5m27wkuFZYKP3Jb68oLHA1GsUukxWs8"
+//"5wSMdEYxSW7iB3rdE7c8yB3bqBUrVtvggjDdY5viyDLk"
